d1debaab0e37db41832446046a5890e9b6529345
AltrOmega <michal.szalachowski.33@gmail.com>
2025-02-25 14:18:10 +0100

    add cli compatibility

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from stats import get_book_text, get_num_words, get_char_count, dict_sort
-
-FRANK = "books/frankenstein.txt"
-TEXT = get_book_text(filepath=FRANK)
+import sys
 
 def main():
-    count_gen = get_num_words(TEXT)
-    count_spec = dict_sort(get_char_count(TEXT))
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    try:
+        path = sys.argv[1]
+        text = get_book_text(path) 
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+
+
+    count_gen = get_num_words(text)
+    count_spec = dict_sort(get_char_count(text))
     count_spec.reverse()
-    path = FRANK
+
+
     print(f'''============ BOOKBOT ============
 Analyzing book found at {path}...
 ----------- Word Count ----------
@@ -15,9 +26,11 @@ Found {count_gen} total words
 --------- Character Count -------''')
     for dict_ in count_spec:
         key, value = list(dict_.items())[0]
-        print(f"{key}: {value}")
-
+        if key.isalpha():
+            print(f"{key}: {value}")
     print("============= END ===============")
     
+
+
 if __name__ == "__main__":
     main()
