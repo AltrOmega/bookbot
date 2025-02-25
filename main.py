0f518cce3dfcce9d7fa408ead2a518c275c77256
AltrOmega <michal.szalachowski.33@gmail.com>
2025-02-25 13:46:40 +0100

    finish basic func

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-FRANK = "books/frankenstein.txt"
+from stats import get_book_text, get_num_words, get_char_count, dict_sort
 
-def get_book_text(filepath: str):
-    with open(filepath, "r", encoding='utf-8') as f:
-        return f.read()
-    
+FRANK = "books/frankenstein.txt"
+TEXT = get_book_text(filepath=FRANK)
+
 def main():
-    print(get_book_text(filepath=FRANK))
+    count_gen = get_num_words(TEXT)
+    count_spec = dict_sort(get_char_count(TEXT))
+    count_spec.reverse()
+    path = FRANK
+    print(f'''============ BOOKBOT ============
+Analyzing book found at {path}...
+----------- Word Count ----------
+Found {count_gen} total words
+--------- Character Count -------''')
+    for dict_ in count_spec:
+        key, value = list(dict_.items())[0]
+        print(f"{key}: {value}")
+
+    print("============= END ===============")
     
 if __name__ == "__main__":
     main()
